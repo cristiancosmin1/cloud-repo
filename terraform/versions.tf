fb62a7fe22c50659761b0dd8cf530aa9fb54e356
cristiancosmin <cristian.cosmin.flutur@gmail.com>
2026-09-13 15:24:34 +0300

    Configure S3 Terraform remote backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,15 @@
 terraform {
   required_version = ">= 1.15.0"
 
-  required_providers {
+  backend "s3" {
+    bucket       = "devops-levelup-terraform-state-691862618786"
+    key          = "staging/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
@@ -17,10 +24,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 3.1"
     }
+
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
-
   }
 }
